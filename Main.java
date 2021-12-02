a02bf3146bed78ab58418043e594a1d51d5fd851
AMK300 <94754949+AMK300@users.noreply.github.com>
2021-12-02 21:56:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+
+  public static void main(String[] args) {
+   System.out.println("Hello world"); 
+
+   String myName = "Ahmed";
+   
+
+   System.out.println(myName);
+   
+   int myAge = 29;
+   System.out.println(myAge);
+
+   char favLetter = 'A';
+   System.out.println(favLetter);
+
+   boolean amIsmart = true;
+ 
+   System.out.println(amIsmart);
+  
+   double starRating = 4.5; 
+   System.out.println(starRating);
+  }
+}
